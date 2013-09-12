d186ecf5ea80c87e0bb44447d51315e284bf8ab2
Sotiris Katsaniotis <sotiris.katsaniotis@gmail.com>
2013-09-12 19:45:49 +0300

    Added md5 hash and file writing

--- run.php
+++ run.php
@@ -1,6 +1,7 @@
 <?php
-
+//solution to return *.html files
 function recursiveGlob($dir, $ext) {
+    $array = array();
     $globFiles = glob("$dir/*.$ext");
     $globDirs  = glob("$dir/*", GLOB_ONLYDIR);
 
@@ -9,12 +10,26 @@ function recursiveGlob($dir, $ext) {
     }
 
     foreach ($globFiles as $file) {
-        print "$file\n"; // Replace '\n' with '<br />' if outputting to browser
+        if(!in_array($file,$array)) {
+            array_push($array,$file);
+        }
     }
+    return $array;
+}
+//only use in CLI mode because of extra large buffer and execution time
+//print_r(recursiveGlob("/home/sotiris-k/*","html"));
+
+$files = recursiveGlob("/home/sotiris-k/*","html");
+
+$myFile = "testFile.txt";
+
+$fh = fopen($myFile, 'w') or die("can't open file");
+
+foreach ($files as $file) {
+    $stringData = md5_file($file)."\n";
+    fwrite($fh, $stringData);
 }
 
-
-//only use in CLI mode because of extra large buffer and execution time
-recursiveGlob("/*","exe");
+fclose($fh);
 
 ?>
